8fac77e590456b1ab2bc55413e0ab459116e5379
KhataiRza <xetai.rza@gmail.com>
2022-03-26 15:37:37 +0000

    xetai added comment

--- spark-postgres.py
+++ spark-postgres.py
@@ -1,4 +1,5 @@
 #03262022 Ramiz
+#03262022 Xetai
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
